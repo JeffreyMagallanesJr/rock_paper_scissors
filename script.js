b9308e52c4ba7ec83da842236b4b635743bda887
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-08-07 06:03:25 +0800

    Add pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("My Rock, Paper, Scissors Program \n by Jeffrey");
+
+/*
+function getComputerChoice
+    return random(rock,paper,scissors)
+
+function singleRound(playerSelection,computerSelection)
+    if playerSelection == rock AND computerSelection == paper
+        print "You Lose! Paper beats Rock!"
+    else if playerSelection == rock AND computerSelection == scissors
+        print "You Win! Rock beats Scissors!"
+    else if playerSelection == rock AND computerSelection == rock
+        print "It's a draw!"
+    else if playerSelection == paper AND computerSelection == rock
+        print "You Win! Paper beats Rock!"
+    else if playerSelection == paper AND computerSelection == scissors
+        print "You Lose! Scissors beats Paper!"
+    else if playerSelection == paper AND computerSelection == paper
+        print "It's a draw!"
+    else if playerSelection == scissors AND computerSelection == rock
+        print "You Lose! Rock beats Scissors!"
+    else if playerSelection == scissors AND computerSelection == paper
+        print "You Win! Scissors beats Paper!"
+    else if playerSelection == scissors AND computerSelection == scissors
+        print "It's a draw!"
+*/
