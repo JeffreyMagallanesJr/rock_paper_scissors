45cf511567459c64eee94fdce2032e4f2a199932
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-08-08 06:13:03 +0800

    Add scoring system in pseudocode

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 console.log("My Rock, Paper, Scissors Program \n by Jeffrey");
 
 /*
-function getComputerChoice
+function getComputerChoice()
     return random(rock,paper,scissors)
 
 function playRound(playerSelection,computerSelection)
     if playerSelection == rock AND computerSelection == paper
         print "You Lose! Paper beats Rock!"
+        computerScore = computerScore + 1
     else if playerSelection == rock AND computerSelection == scissors
         print "You Win! Rock beats Scissors!"
+        playerScore = playerScore + 1
     else if playerSelection == rock AND computerSelection == rock
         print "It's a draw!"
     else if playerSelection == paper AND computerSelection == rock
         print "You Win! Paper beats Rock!"
+        playerScore = playerScore + 1
     else if playerSelection == paper AND computerSelection == scissors
         print "You Lose! Scissors beats Paper!"
+        computerScore = computerScore + 1
     else if playerSelection == paper AND computerSelection == paper
         print "It's a draw!"
     else if playerSelection == scissors AND computerSelection == rock
         print "You Lose! Rock beats Scissors!"
+        computerScore = computerScore + 1
     else if playerSelection == scissors AND computerSelection == paper
         print "You Win! Scissors beats Paper!"
+        playerScore = playerScore + 1
     else if playerSelection == scissors AND computerSelection == scissors
         print "It's a draw!"
 
+function displayScore()
+    print "Your score is {playerScore}"
+    print "Computer's Score is {computerScore}"
+
+function game()
+    playRound()
+    displayScore()
+    playRound()
+    displayScore()
+    playRound()
+    displayScore()
+    playRound()
+    displayScore()
+    playRound()
+    displayScore()
+
 playerSelection = input("Rock, Paper or Scissors?")
 computerSelection = getComputerChoice()
+playerScore = 0
+computerScore = 0
 
 
 */
