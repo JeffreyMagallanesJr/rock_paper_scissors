bac5178347f8183d940add1ce55dd908cfabc230
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-08-08 06:37:03 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -65,3 +65,28 @@ function getComputerChoice() {
 
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        return "It's a draw!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock!"
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors!"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock!"
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper!"
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors!"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper!"
+    }
+}
+
+let playerSelection = "RoCk";
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
