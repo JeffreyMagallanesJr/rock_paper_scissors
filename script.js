61553bfdada8089f673aea091769f96a368ca651
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-09-05 23:17:36 +0800

    Get display working properly

--- script.js
+++ script.js
@@ -30,21 +30,27 @@ function playRound(selection) {
         results.innerText += "\nIt's a draw!";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore += 1;
+        computer_score_display.innerText = `Computer Score: ${computerScore}`;
         results.innerText += "\nYou Lose! Paper beats Rock!";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore += 1;
+        player_score_display.innerText = `Player Score: ${playerScore}`;
         results.innerText += "\nYou Win! Rock beats Scissors!";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore += 1;
+        player_score_display.innerText = `Player Score: ${playerScore}`;
         results.innerText += "\nYou Win! Paper beats Rock!";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore += 1;
+        computer_score_display.innerText = `Computer Score: ${computerScore}`;
         results.innerText += "\nYou Lose! Scissors beats Paper!";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore += 1;
+        computer_score_display.innerText = `Computer Score: ${computerScore}`;
         results.innerText += "\nYou Lose! Rock beats Scissors!";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore += 1;
+        player_score_display.innerText = `Player Score: ${playerScore}`;
         results.innerText += "\nYou Win! Scissors beats Paper!";
     }
 }
@@ -74,6 +80,8 @@ const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const results = document.querySelector('.results');
+let player_score_display = document.querySelector('.playerScoreDisplay');
+let computer_score_display = document.querySelector('.computerScoreDisplay');
 
 rock.addEventListener('click', () => playRound("rock"));
 paper.addEventListener('click', () => playRound("paper"));
